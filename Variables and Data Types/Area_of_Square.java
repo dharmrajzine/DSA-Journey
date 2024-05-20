437c41f88e01716c5b30f0bdd0d7f9cb59041108
dharmrajzine <dharmrajzine@gmail.com>
2024-05-20 19:23:09 +0530

    Area of Square Program Added

---
+++ Variables and Data Types/Area_of_Square.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Area_of_Square {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number you want area of square : ");
+        int side = sc.nextInt();
+
+        int area = side * side;
+
+        System.out.println("Area of the square is : " + area);
+    }
+}
