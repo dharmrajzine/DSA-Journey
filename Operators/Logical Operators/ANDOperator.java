e6a3218945aa380a12c2d299db594488bdf0ee0c
dharmrajzine <dharmrajzine@gmail.com>
2024-05-22 14:44:20 +0530

    Logical Operators Programs Added

---
+++ Operators/Logical Operators/ANDOperator.java
@@ -0,0 +1,17 @@
+
+public class ANDOperator {
+    public static void main(String[] args) {
+
+        int a = 10, b = 20, c = 20, d = 0;
+
+        System.out.println("Var1 = " + a);
+        System.out.println("Var2 = " + b);
+        System.out.println("Var3 = " + c);
+
+        if ((a < b) && (b == c)) {
+            d = a + b + c;
+            System.out.println("This sum is: " + d);
+        } else
+            System.out.println("False conditions");
+    }
+}
