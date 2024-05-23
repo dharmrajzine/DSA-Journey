360b77cf5dd0def57e9f11ac2b6ecf818ecb4022
dharmrajzine <dharmrajzine@gmail.com>
2024-05-23 18:29:50 +0530

    updated

---
+++ Conditional Statements/If-Else/If_ElseEx_five.java
@@ -0,0 +1,11 @@
+
+public class If_ElseEx_five {
+    public static void main(String[] args) {
+
+        String lang = "Java";
+
+        if (lang == "Java") {
+            System.out.println("Best programming language");
+        }
+    }
+}
