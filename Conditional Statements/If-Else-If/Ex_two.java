840da61c2347606b70ce51facd3edf7677c3eb3b
dharmrajzine <dharmrajzine@gmail.com>
2024-05-23 22:16:06 +0530

    If-Else-If programs added

---
+++ Conditional Statements/If-Else-If/Ex_two.java
@@ -0,0 +1,22 @@
+
+public class Ex_two {
+
+    public static void main(String[] args) {
+
+        int i = 20;
+
+        if (i == 10)
+            System.out.println("i is 10\n");
+
+        else if (i == 15)
+            System.out.println("i is 15\n");
+
+        else if (i == 20)
+            System.out.println("i is 20\n");
+
+        else
+            System.out.println("i is not present\n");
+
+        System.out.println("Outside if-else-if");
+    }
+}
