5db8f29d5a6809c9cbed2cc4a5e5941753d4793d
dharmrajzine <dharmrajzine@gmail.com>
2024-07-08 22:41:17 +0530

    practice

---
+++ Pattern02/HallowPattern.java
@@ -0,0 +1,20 @@
+ package Pattern02;
+
+public class HallowPattern {
+    public static void hallow_Reactangle(int totRows, int totCols) {
+        for (int i = 1; i <= totRows; i++) {
+            for (int j = 1; j <= totCols; j++) {
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.err.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        hallow_Reactangle(4, 5);
+    }
+}
