d67da5554ff3196a59366b21bc909a215857f951
dharmrajzine <dharmrajzine@gmail.com>
2024-07-15 13:53:54 +0530

    Patterns 1. Hollow Rhombus 2. Diamond program

---
+++ Pattern02/Pattern_09.java
@@ -0,0 +1,28 @@
+package Pattern02;
+
+public class Pattern_09 {
+    public static void diamond(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        diamond(5);
+    }
+}
